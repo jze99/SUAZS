48a1f3ec2a3ce0307033fd49f79b6ff4ead4fd89
jze9 <118809237+jze99@users.noreply.github.com>
2024-03-16 01:56:24 +0500

    finish

--- receipts.py
+++ receipts.py
@@ -1,6 +1,6 @@
 class Receipts():
     
-    def __init__(self, cost, cost_per_liter, view_fuel, manufacturer_fuel, data, time, liters):
+    def __init__(self, cost, cost_per_liter, view_fuel, manufacturer_fuel, data, time, liters, column_name):
         
         self.cost = cost
         self.cost_per_liter = cost_per_liter
@@ -9,16 +9,18 @@ class Receipts():
         self.data = data
         self.time = time
         self.liters = liters
+        self.column_name = column_name
         
         self.type_fuel = str(self.view_fuel)+": "+str(self.manufacturer_fuel)
         
 class ReceiptsGroop():
     
-    def __init__(self, type_fuel):
+    def __init__(self, type_fuel = None, column_name = None):
         
         self.receipts = []
         self.type_fuel = type_fuel
-    
+        self.column_name = column_name
+            
     def AddReceipts(self,receipts):
         
         self.receipts.append(receipts)
--- setingHome.py
+++ setingHome.py
@@ -48,9 +48,10 @@ class SetingHome(UserControl):
         
         self.list_receipts = []
         self.list_receipts_groop = []
+        self.list_receipts_column_group = []
         self.list_receipts_temp = None
         
-        self.max_sales_id = None
+        self.lit_cost = 0.0
         
         self.earnings_statistics = BarChart(
             interactive=True,
@@ -68,12 +69,27 @@ class SetingHome(UserControl):
             ),
         )
         
+        self.type_diogram = Dropdown(
+        expand=True,
+        on_change=self.UpdateTypeData,
+        value="Количество продаж по типу топлива",
+        options=[
+            dropdown.Option("Количество продаж по типу топлива"),
+            dropdown.Option("Количество продаж по названию колонки"),
+            dropdown.Option("Количество денег с каждого типа топлива"),
+            dropdown.Option("Количество денег с каждой колонки"),
+            dropdown.Option("Количество литров с каждого типа топлива"),
+            dropdown.Option("Количество литров с каждой колонки"),
+        ],
+    )
+        
         self.column_main = Column(
             controls=[
+                
                 Row(
                     alignment=MainAxisAlignment.CENTER,
                     controls=[
-                        Text(value="Общее количество продаж топлива", size=40, expand=True, )
+                        self.type_diogram
                     ]
                 ),
                 Row(
@@ -85,6 +101,11 @@ class SetingHome(UserControl):
         )
         
         self.LoadDataReceipts()
+        
+    def UpdateTypeData(self, e):
+        self.earnings_statistics.bar_groups.clear()
+        self.LoadBarChart()
+        self.earnings_statistics.update()
         
     def LoadDataReceipts(self):
         from dataFuel import data_base
@@ -103,26 +124,27 @@ class SetingHome(UserControl):
                     manufacturer_fuel=data[4],
                     data=data[5],
                     time=data[6],
-                    liters=data[7]
+                    liters=data[7],
+                    column_name=data[8]
                 )
             )
             
         self.list_receipts_temp = self.list_receipts[:]
         
         self.list_receipts_groop = self.LoadReceiptsGroop(self.list_receipts)
-        self.MaxSales()
+        self.list_receipts_column_group = self.LoadReceiptsGroopColumnName(self.list_receipts)
         self.LoadBarChart()
         
-    def MaxSales(self):
+    def MaxSales(self, list):
         max_count = 0
-        if not self.list_receipts_groop:
+        if not list:
             return
-        for i_receipts, receipts_groop in enumerate(self.list_receipts_groop):
+        for i_receipts, receipts_groop in enumerate(list):
             count = len(receipts_groop.receipts)  # Получаем количество элементов в текущем массиве
             if count > max_count:
                 max_count = count
                 id_max = i_receipts
-        self.max_sales_id=id_max
+        return id_max
     
     def LoadReceiptsGroop(self, receipts_list):
         from receipts import ReceiptsGroop
@@ -130,8 +152,21 @@ class SetingHome(UserControl):
         for receipt in receipts_list:
             fuel_type = receipt.type_fuel
             if fuel_type not in receipts_groop_dict:
-                receipts_groop_dict[fuel_type] = ReceiptsGroop(fuel_type)
+                receipts_groop_dict[fuel_type] = ReceiptsGroop(type_fuel = fuel_type)
             receipts_groop_dict[fuel_type].AddReceipts(receipts=receipt)
+
+        list_receipts_groop = list(receipts_groop_dict.values())
+
+        return list_receipts_groop
+    
+    def LoadReceiptsGroopColumnName(self, receipts_list):
+        from receipts import ReceiptsGroop
+        receipts_groop_dict = {}
+        for receipt in receipts_list:
+            column_name = receipt.column_name
+            if column_name not in receipts_groop_dict:
+                receipts_groop_dict[column_name] = ReceiptsGroop(column_name = column_name)
+            receipts_groop_dict[column_name].AddReceipts(receipts=receipt)
 
         list_receipts_groop = list(receipts_groop_dict.values())
 
@@ -140,29 +175,117 @@ class SetingHome(UserControl):
     def LoadBarChart(self):
         if not self.list_receipts_groop:
             return
-        self.earnings_statistics.max_y=int(len(self.list_receipts_groop[self.max_sales_id].receipts))+2
-        for i_receipts_groop, receipts_groop in enumerate(self.list_receipts_groop):
-            self.earnings_statistics.bar_groups.append(
-                BarChartGroup(
-                    x=i_receipts_groop,
-                    bar_rods=[
-                        BarChartRod(
-                            from_y=0,
-                            to_y=len(receipts_groop.receipts),
-                            width=10,
-                            color=self.colors[i_receipts_groop],
-                            tooltip=len(receipts_groop.receipts),
-                            border_radius=0,
-                         ),
-                    ],
+        
+        lit_cost =[]
+        temp = 0
+        
+        match  self.type_diogram.value:
+            case "Количество продаж по типу топлива":
+                list_data = self.list_receipts_groop
+                self.earnings_statistics.max_y=int(len(list_data[self.MaxSales(list_data)].receipts))+2
+                
+            case "Количество продаж по названию колонки":
+                list_data = self.list_receipts_column_group
+                self.earnings_statistics.max_y=int(len(list_data[self.MaxSales(list_data)].receipts))+2
+                
+            case "Количество денег с каждого типа топлива":
+                list_data = self.list_receipts_groop
+                for cost in list_data:
+                    temp = 0
+                    for co in cost.receipts:
+                        temp += co.cost
+                    lit_cost.append(temp)
+                self.earnings_statistics.max_y=round(max(lit_cost),0)+10
+                self.earnings_statistics.update()
+            case "Количество денег с каждой колонки":
+                list_data = self.list_receipts_column_group
+                for cost in list_data:
+                    temp = 0
+                    for co in cost.receipts:
+                        temp += co.cost
+                    lit_cost.append(temp)
+                self.earnings_statistics.max_y=round(max(lit_cost),0)+10
+                self.earnings_statistics.update()
+            case "Количество литров с каждого типа топлива":
+                list_data = self.list_receipts_groop
+                for cost in list_data:
+                    temp = 0
+                    for co in cost.receipts:
+                        temp += co.liters
+                    lit_cost.append(temp)
+                self.earnings_statistics.max_y=round(max(lit_cost),0)+10
+                self.earnings_statistics.update()
+            case "Количество литров с каждой колонки":
+                list_data = self.list_receipts_column_group
+                for cost in list_data:
+                    temp = 0
+                    for co in cost.receipts:
+                        temp += co.liters
+                    lit_cost.append(temp)
+                self.earnings_statistics.max_y=round(max(lit_cost),0)+10
+                self.earnings_statistics.update()
+        if not lit_cost:
+            for i_receipts_groop, receipts_groop in enumerate(list_data):
+                self.earnings_statistics.bar_groups.append(
+                    BarChartGroup(
+                        x=i_receipts_groop,
+                        bar_rods=[
+                            BarChartRod(
+                                from_y=0,
+                                to_y=len(receipts_groop.receipts),
+                                width=10,
+                                color=self.colors[i_receipts_groop],
+                                tooltip=len(receipts_groop.receipts),
+                                border_radius=0,
+                             ),
+                        ],
+                    )
                 )
-            )
-            self.earnings_statistics.bottom_axis.labels.append(
-                ChartAxisLabel(
-                    value=i_receipts_groop,
-                    label=Container(Text(value=str(receipts_groop.type_fuel)))
-                )   
-            )
+                if receipts_groop.type_fuel != None:
+                    self.earnings_statistics.bottom_axis.labels.append(
+                        ChartAxisLabel(
+                            value=i_receipts_groop,
+                            label=Container(Text(value=str(receipts_groop.type_fuel)))
+                        )   
+                    )
+                if receipts_groop.column_name != None:
+                    self.earnings_statistics.bottom_axis.labels.append(
+                        ChartAxisLabel(
+                            value=i_receipts_groop,
+                            label=Container(Text(value=str(receipts_groop.column_name)))
+                        )   
+                    )         
+        if lit_cost:
+            for i_receipts_groop, receipts_groop in enumerate(list_data):
+                self.earnings_statistics.bar_groups.append(
+                    BarChartGroup(
+                        x=i_receipts_groop,
+                        bar_rods=[
+                            BarChartRod(
+                                from_y=0,
+                                to_y=round(lit_cost[i_receipts_groop],2),
+                                width=10,
+                                color=self.colors[i_receipts_groop],
+                                tooltip=round(lit_cost[i_receipts_groop],2),
+                                border_radius=0,
+                             ),
+                        ],
+                    )
+                )
+                if receipts_groop.type_fuel != None:
+                    self.earnings_statistics.bottom_axis.labels.append(
+                        ChartAxisLabel(
+                            value=i_receipts_groop,
+                            label=Container(Text(value=str(receipts_groop.type_fuel)))
+                        )   
+                    )
+                if receipts_groop.column_name != None:
+                    self.earnings_statistics.bottom_axis.labels.append(
+                        ChartAxisLabel(
+                            value=i_receipts_groop,
+                            label=Container(Text(value=str(receipts_groop.column_name)))
+                        )   
+                    )         
         pass
         
     def build(self):
